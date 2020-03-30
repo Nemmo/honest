8b8421660c2738eec5bd2b19eeb49f3fb3ad4af0
Alimzhan Abuov <alimzhan.abu@yandex.kz>
2020-03-30 08:48:06 +0600

    feat(arch): enable webpack module hot reload

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,16 @@ import { NestFactory } from '@nestjs/core';
 
 import AppModule from 'src/app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
